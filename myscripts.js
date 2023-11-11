5d8df37c510c85723bdbb7a203a2675d98c7a494
Khai <muankhai10@gmail.com>
2023-11-11 16:18:44 -0500

    commit

---
+++ myscripts.js
@@ -0,0 +1,18 @@
+// Creating a string
+const greeting = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+//Display the string
+console.log(greeting);
+
+// Generate variables with calculated values
+const firstCode  = 7 + 3;
+const secondCode  = 360 / 9;
+const thirdCode = 3 * 13;
+
+// Combine the codes into a string
+const combination = `${firstCode} - ${secondCode} - ${thirdCode}`;
+
+// Display the combination
+console.log("The combination of the lock is " + firstCode + "-" + secondCode  + "-" + thirdCode + ".");
+
+// Display the combination in an alert popup
+alert(combination);
